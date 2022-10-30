3062054968e7141e7dc659e0f0010617c3e31ffe
ume20s <ume20s@gmail.com>
2022-10-30 15:36:08 +0900

    Initial commit

--- app/src/main/java/com/ume20studio/tapmanben/MainActivity.kt
+++ app/src/main/java/com/ume20studio/tapmanben/MainActivity.kt
@@ -4,13 +4,16 @@ import android.animation.ObjectAnimator
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     // BGM再生用メディアプレーヤーのインスタンス
     private lateinit var mpop:MediaPlayer
-    private var mpst = ArrayList<MediaPlayer>()
-    private val stbgm = arrayOf(R.raw.s01, R.raw.s02, R.raw.s03, R.raw.s04,
-        R.raw.s05, R.raw.s06, R.raw.s07, R.raw.s08, R.raw.s09, R.raw.s10 )
-
-//    private val stbgm = arrayOf(R.raw.s01_15, R.raw.s01_15, R.raw.s01_15, R.raw.s01_15,
-//        R.raw.s01_15, R.raw.s01_15, R.raw.s01_15, R.raw.s01_15, R.raw.s01_15, R.raw.s01_15 )
+    private lateinit var mp:MediaPlayer
 
     // 音声再生用サウンドプールのインスタンス
     private lateinit var soundPool: SoundPool
@@ -38,10 +36,13 @@ class MainActivity : AppCompatActivity() {
     private var score:Int = 0               // スコア
     private var highscore:Int = 0           // ハイスコア
     private var level:Int = 0               // ステージレベル
+    private var isGaming:Boolean = false    // ゲーム進行中フラグ
 
-    // ステージレベル別のポイントと間隔
+    // ステージレベル別のポイントとBGM速度、間隔
     private var point = arrayOf(10,20,30,40,60,80,100,120,150,200)
-    private var interval = arrayOf(100, 90, 80, 70, 60, 50, 40, 30, 20, 10)
+    private var bgmspeed = arrayOf(0.80f, 0.90f, 1.00f, 1.05f, 1.10f, 1.15f, 1.20f, 1.25f, 1.30f, 1.40f)
+    private var interval = arrayOf(100, 90, 80, 70, 60, 50, 50, 40, 40, 50)
+    private var ttlMax = arrayOf(300, 250, 200, 150, 120, 100, 80, 40, 20, 10)
     private var remain:Int = interval[level]
 
     // ステージ定数
@@ -67,33 +68,30 @@ class MainActivity : AppCompatActivity() {
         R.id.ImgBtn4, R.id.ImgBtn5, R.id.ImgBtn6, R.id.ImgBtn7, R.id.ImgBtn8)
 
     // お弁当おにぎりのイメージ配列
-    val bento = arrayOf(R.drawable.plain_test, R.drawable.onigiri_test, R.drawable.bento_test)
-    val bentomaru = arrayOf(R.drawable.plain_test, R.drawable.onigiri_maru_test, R.drawable.bento_maru_test)
-    val bentopeke = arrayOf(R.drawable.plain_test, R.drawable.onigiri_peke_test, R.drawable.bento_peke_test)
+    val bento = arrayOf(R.drawable.plain, R.drawable.onigiri, R.drawable.obento)
+    val bentomaru = arrayOf(R.drawable.plain, R.drawable.onigiri_maru, R.drawable.obento_maru)
+    val bentopeke = arrayOf(R.drawable.plain, R.drawable.onigiri_peke, R.drawable.obento_peke)
 
     // パネルの表示状況関連
     private var alive = arrayOf(0,0,0,0,0,0,0,0,0)
     private var ttl = arrayOf(0,0,0,0,0,0,0,0,0)
     private var alivenum:Int = 0
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         // もとからある初期化
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // オープニングBGMスタート
-        mpop = MediaPlayer.create(this,R.raw.manzoku)
+        mpop = MediaPlayer.create(this,R.raw.openingbgm)
         mpop.isLooping = true
         mpop.seekTo(0)
         mpop.start()
 
         // ステージBGMの準備
-        for(i in 0 until 10) {
-            val mp = MediaPlayer.create(this, stbgm[i])
-            mpst.add(mp)
-            mpst[i].isLooping = false
-            mpst[i].setOnCompletionListener(PlayerCompletionListener())
-        }
+        mp = MediaPlayer.create(this,R.raw.stagebgm_test)
+        mp.setOnCompletionListener(PlayerCompletionListener())
 
         // サウンドプールのもろもろの初期化
         val sPattr = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME)
@@ -119,10 +117,15 @@ class MainActivity : AppCompatActivity() {
         for(i in 0 until 9) {
             findViewById<ImageButton>(panel[i]).setOnClickListener(panelListener)
         }
+
+        // ステージクリア画像へのイベントリスナの紐づけ
+        val clearListener = ClearTap()
+        findViewById<ImageButton>(R.id.stageclearButton).setOnClickListener(clearListener)
     }
 
     // ゲームスタート時の処理
     private inner class StartTap : View.OnClickListener {
+        @RequiresApi(Build.VERSION_CODES.M)
         override fun onClick(v: View) {
             // スタートボタンを非表示に
             findViewById<ImageButton>(R.id.startButton).visibility = View.INVISIBLE
@@ -145,12 +148,15 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
+
+                // ステージ宣言してカウントダウン画像を非表示に
                 soundPool.play(vstage[stage], 1.0f, 1.0f, 0, 0, 1.0f)
                 findViewById<ImageView>(R.id.countdownImage).visibility = View.INVISIBLE
 
                 // ゲームBGMスタート
-                mpst[level].seekTo(0)
-                mpst[level].start()
+                mp.setPlaybackParams(mp.getPlaybackParams().setSpeed(1.001f))
+                mp.start()
+                mp.setPlaybackParams(mp.getPlaybackParams().setSpeed(bgmspeed[level]))
             })
             thread.start()
 
@@ -159,6 +165,7 @@ class MainActivity : AppCompatActivity() {
             vTimer!!.schedule(object : TimerTask() {
                 override fun run() {
                     vHandler.post {
+                        // 残り時間減算
                         remain--
 
                         // パネル表示リミットがきたら消す
@@ -176,24 +183,26 @@ class MainActivity : AppCompatActivity() {
                         // インターバル毎にランダムにパネルを表示
                         if(remain <=0) {
                             for (i in 0..8) {
-                                // タップされているパネルを消去
+                                // タップ済みのパネルを消去
                                 if(alive[i] == taped) {
                                     findViewById<ImageButton>(panel[i]).setImageResource(bento[pla])
                                     alive[i] = pla
                                     alivenum--
                                 }
                             }
+
+                            // 表示パネルは最大４枚にする
                             if(alivenum < 4) {
-                                val alivePanel = (0..8).random()
+                                val alivePanel = Random.nextInt(9)
                                 if(alive[alivePanel] == pla) {
-                                    if((0..1).random() == 0){
+                                    if(Random.nextInt(2) == 0){
                                         findViewById<ImageButton>(panel[alivePanel]).setImageResource(bento[oni])
                                         alive[alivePanel] = oni
                                     } else {
                                         findViewById<ImageButton>(panel[alivePanel]).setImageResource(bento[ben])
                                         alive[alivePanel] = ben
                                     }
-                                    ttl[alivePanel] = interval[level]
+                                    ttl[alivePanel] = ttlMax[level]
                                     alivenum++
                                 }
                             }
@@ -204,106 +213,130 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }, 3800, 10)
+
+            // ゲーム進行中フラグをtrueに
+            isGaming = true
         }
     }
 
     // パネルがタップされた時の処理
     private inner class PanelTap : View.OnClickListener {
         override fun onClick(v: View){
+
             var pp:Int = 0
-
-            when(v.id){
-                panel[0] -> {
-                    pp = 0
-                }
-                panel[1] -> {
-                    pp = 1
-                }
-                panel[2] -> {
-                    pp = 2
-                }
-                panel[3] -> {
-                    pp = 3
-                }
-                panel[4] -> {
-                    pp = 4
-                }
-                panel[5] -> {
-                    pp = 5
-                }
-                panel[6] -> {
-                    pp = 6
-                }
-                panel[7] -> {
-                    pp = 7
-                }
-                panel[8] -> {
-                    pp = 8
-                }
-            }
-            when(alive[pp]) {
-                oni -> {
-                    if(stage == nene) {
-                        findViewById<ImageButton>(panel[pp]).setImageResource(bentomaru[oni])
-                        soundPool.play(vpinpon, 1.0f, 1.0f, 0, 0, 1.0f)
-                        score += point[level]
-                    } else {
-                        findViewById<ImageButton>(panel[pp]).setImageResource(bentopeke[oni])
-                        soundPool.play(vbubuu, 1.0f, 1.0f, 0, 0, 1.0f)
-                        score -= point[level] / 2
+            // ゲーム進行中のみ処理
+            if(isGaming) {
+                when(v.id){
+                    panel[0] -> {
+                        pp = 0
                     }
-                    alive[pp] = taped
-                }
-                ben -> {
-                    if(stage == coco) {
-                        findViewById<ImageButton>(panel[pp]).setImageResource(bentomaru[ben])
-                        soundPool.play(vpinpon, 1.0f, 1.0f, 0, 0, 1.0f)
-                        score += point[level]
-                    } else {
-                        findViewById<ImageButton>(panel[pp]).setImageResource(bentopeke[ben])
-                        soundPool.play(vbubuu, 1.0f, 1.0f, 0, 0, 1.0f)
-                        score -= point[level] / 2
+                    panel[1] -> {
+                        pp = 1
                     }
-                    alive[pp] = taped
+                    panel[2] -> {
+                        pp = 2
+                    }
+                    panel[3] -> {
+                        pp = 3
+                    }
+                    panel[4] -> {
+                        pp = 4
+                    }
+                    panel[5] -> {
+                        pp = 5
+                    }
+                    panel[6] -> {
+                        pp = 6
+                    }
+                    panel[7] -> {
+                        pp = 7
+                    }
+                    panel[8] -> {
+                        pp = 8
+                    }
                 }
+
+                // タップしたアイテムによって処理がいろいろ
+                when(alive[pp]) {
+                    oni -> {
+                        if(stage == nene) {
+                            findViewById<ImageButton>(panel[pp]).setImageResource(bentomaru[oni])
+                            findViewById<ImageView>(R.id.imageNene).setImageResource(R.drawable.nene1)
+                            soundPool.play(vpinpon, 1.0f, 1.0f, 0, 0, 1.0f)
+                            score += point[level]
+                        } else {
+                            findViewById<ImageButton>(panel[pp]).setImageResource(bentopeke[oni])
+                            findViewById<ImageView>(R.id.imageCoco).setImageResource(R.drawable.coco2)
+                            soundPool.play(vbubuu, 1.0f, 1.0f, 0, 0, 1.0f)
+                            score -= point[level] / 2
+                        }
+                        alive[pp] = taped
+                    }
+                    ben -> {
+                        if(stage == coco) {
+                            findViewById<ImageButton>(panel[pp]).setImageResource(bentomaru[ben])
+                            findViewById<ImageView>(R.id.imageCoco).setImageResource(R.drawable.coco1)
+                            soundPool.play(vpinpon, 1.0f, 1.0f, 0, 0, 1.0f)
+                            score += point[level]
+                        } else {
+                            findViewById<ImageButton>(panel[pp]).setImageResource(bentopeke[ben])
+                            findViewById<ImageView>(R.id.imageNene).setImageResource(R.drawable.nene2)
+                            soundPool.play(vbubuu, 1.0f, 1.0f, 0, 0, 1.0f)
+                            score -= point[level] / 2
+                        }
+                        alive[pp] = taped
+                    }
+                }
+
+                // スコア表示
+                findViewById<TextView>(R.id.Score).text = score.toString()
+                findViewById<TextView>(R.id.HighScore).text = level.toString()
             }
-
-            // スコア表示
-            findViewById<TextView>(R.id.Score).text = score.toString()
-
             // 仮 動作確認用～　ネネココステージチェンジ
-            if(Math.random() > 0.8){
+            if(Random.nextInt(10) > 8){
                 slideStage()
             }
         }
     }
 
+    // ステージクリア画像がタップされた時の処理
+    private inner class ClearTap : View.OnClickListener {
+        override fun onClick(v: View){
+            isGaming = true
+        }
+    }
+
     // BGM終了（つまりステージ終了）時の処理
     private inner class PlayerCompletionListener: MediaPlayer.OnCompletionListener {
+        @RequiresApi(Build.VERSION_CODES.M)
         override fun onCompletion(mp: MediaPlayer?) {
-
-            findViewById<TextView>(R.id.HighScore).setText(level.toString())
-
             val thread = Thread(Runnable {
-                mpst[level].seekTo(0)
-                if(level >= 2) {
-                    level = 0
+                // ゲーム進行中フラグをfalseに
+                isGaming = false
+
+                mp?.seekTo(0)
+                if(level >= 9) {
+                    level = 9
                 } else {
                     level++
                 }
                 try {
                     vHandler.post {
-                        findViewById<ImageView>(R.id.stageclearImage).visibility = View.VISIBLE
+                        findViewById<ImageButton>(R.id.stageclearButton).visibility = View.VISIBLE
                     }
                     soundPool.play(vstageclear, 1.0f, 1.0f, 0, 0, 1.0f)
                     Thread.sleep(2000)
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
-                findViewById<ImageView>(R.id.stageclearImage).visibility = View.INVISIBLE
+                findViewById<ImageView>(R.id.stageclearButton).visibility = View.INVISIBLE
 
                 // ゲームBGMスタート
-                mpst[level].start()
+                mp?.start()
+                mp?.setPlaybackParams(mp.getPlaybackParams().setSpeed(bgmspeed[level]))
+
+                // ゲーム進行中フラグをtrueに
+                isGaming = true
             })
             thread.start()
         }
@@ -355,23 +388,42 @@ class MainActivity : AppCompatActivity() {
     // ポーズでもゲーム終了
     override fun onPause() {
         super.onPause()
-        mpop.release()
-        for(i in 0 until 9) {
-            mpst[i].stop()
-            mpst[i].release()
-        }
-        soundPool.release()
+        val thread = Thread(Runnable {
+            soundPool.release()
+            mpop?.stop()
+            mpop?.release()
+            mp?.stop()
+            mp?.release()
+        })
+        thread.start()
+        finish()
+    }
+
+    // ポーズでもゲーム終了
+    override fun onStop() {
+        super.onStop()
+        val thread = Thread(Runnable {
+            soundPool.release()
+            mpop?.stop()
+            mpop?.release()
+            mp?.stop()
+            mp?.release()
+        })
+        thread.start()
         finish()
     }
 
     // 終了なら音関係のメモリを解放
     override fun onDestroy() {
         super.onDestroy()
-        mpop.release()
-        for(i in 0 until 9) {
-            mpst[i].stop()
-            mpst[i].release()
-        }
-        soundPool.release()
+        val thread = Thread(Runnable {
+            soundPool.release()
+            mpop?.stop()
+            mpop?.release()
+            mp?.stop()
+            mp?.release()
+        })
+        thread.start()
+        finish()
     }
 }
